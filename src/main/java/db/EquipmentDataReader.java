2da32eeaa45fa2d2e842c666d603e55781d1f9dd
Karkkainen Sami <sami.karkkainen@danfoss.com>
2018-01-15 09:57:59 +0200

    Added test version of reading equipment data from excel-file

---
+++ src/main/java/db/EquipmentDataReader.java
@@ -0,0 +1,87 @@
+package db;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class EquipmentDataReader {
+	/*
+	private static final String FILE_NAME = "C:\\EquipmentManager\\Eclipse_workspace\\EquipmentManager\\test_files\\Omaisuusluettelo.xlsx";
+	
+	public static void main(String[] args) {
+
+		try {
+
+			FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+
+			while (iterator.hasNext()) {
+
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+
+				while (cellIterator.hasNext()) {
+
+					Cell currentCell = cellIterator.next();
+					// getCellTypeEnum shown as deprecated for version 3.15
+					// getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+						System.out.print(currentCell.getStringCellValue() + "--");
+					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+						System.out.print(currentCell.getNumericCellValue() + "--");
+					}
+
+				}
+				System.out.println();
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	*/
+	public static void main(String[] args) throws IOException {
+		String excelFilePath = "C:\\\\EquipmentManager\\\\Eclipse_workspace\\\\EquipmentManager\\\\test_files\\\\Omaisuusluettelo.xlsx";
+		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheetAt(0);
+		Iterator<Row> iterator = firstSheet.iterator();
+
+		while (iterator.hasNext()) {
+			Row nextRow = iterator.next();
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+
+				switch (cell.getCellType()) {
+				case Cell.CELL_TYPE_STRING:
+					System.out.print(cell.getStringCellValue());
+					break;
+				case Cell.CELL_TYPE_BOOLEAN:
+					System.out.print(cell.getBooleanCellValue());
+					break;
+				case Cell.CELL_TYPE_NUMERIC:
+					System.out.print(cell.getNumericCellValue());
+					break;
+				}
+				System.out.print(" - ");
+			}
+			System.out.println();
+		}
+
+		//workbook.close();
+		inputStream.close();
+	}
+}
